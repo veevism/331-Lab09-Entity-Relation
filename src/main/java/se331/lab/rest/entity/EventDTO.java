22cbcf2bba1bf6cc9eb54fdb4185df100ab49baf
sorawee <veevisorawee@gmail.com>
2023-10-05 01:02:11 +0700

    3.2

---
+++ src/main/java/se331/lab/rest/entity/EventDTO.java
@@ -0,0 +1,22 @@
+package se331.lab.rest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventDTO {
+    Long id;
+    String category;
+    String title;
+    String description;
+    String location;
+    String date;
+    String time;
+    Boolean petAllowed;
+    EventOrganizerDTO organizer;
+}
---
+++ src/main/java/se331/lab/rest/entity/EventOrganizerDTO.java
@@ -0,0 +1,15 @@
+package se331.lab.rest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventOrganizerDTO {
+    Long id;
+    String name;
+}
